6b1a04c149f5bed012d9bafbf0b92ae1926efb2b
Atlan <alavutdin_atlan@mail.ru>
2022-12-18 22:17:09 +0300

    Add Task2

---
+++ Homework1/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Enter the first number");
+int numberA = int.Parse(ReadLine()!);
+WriteLine("Enter the second number");
+int numberB = int.Parse(ReadLine()!);
+int max = numberA;
+
+if (max>numberB)
+{
+    WriteLine(max);
+}
+else
+{
+    max = numberB;
+    WriteLine (max);
+}
