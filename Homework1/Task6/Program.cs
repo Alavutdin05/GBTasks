96f15facde8e4626b15bde6f62c2ed1ccf783d94
Atlan <alavutdin_atlan@mail.ru>
2022-12-18 22:30:14 +0300

    Add Task6

---
+++ Homework1/Task6/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Enter a number:");
+int number = int.Parse(ReadLine()!);
+
+if (number % 2 == 0)
+{
+    WriteLine("Yes");
+}
+else
+{
+    WriteLine("No");
+}
+
