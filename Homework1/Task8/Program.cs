015f1f0b8cc09c1958003ddef608ac239f65aeff
Atlan <alavutdin_atlan@mail.ru>
2022-12-18 22:34:40 +0300

    Add Task8

---
+++ Homework1/Task8/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Enter a number:");
+int number = int.Parse(ReadLine()!);
+int count = 2;
+
+while (count<=number)
+{
+    if (count % 2 == 0)
+    {
+        WriteLine(count);
+    }
+    count++;
+}
